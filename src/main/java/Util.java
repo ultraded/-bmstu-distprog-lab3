f95cf7222913a1f9d0203a5c61377258829b7930
ultraded <andrey.chernenkov@mail.ru>
2021-12-25 01:46:35 +0300

    Scripted auto-commit on change (2021-12-25 01:46:35) by gitwatch.sh

--- src/main/java/Util.java
+++ src/main/java/Util.java
@@ -1,3 +1,5 @@
 final public class Util {
-    private static 
+    private static final String DELIMITER = ",";
+    private static final int AIPORT_ID_INDEX = 14, DELAY_INDEX = 17;
+    private static final boolean FLIGHT_WC_IDENTIFICATION = false;
 }
