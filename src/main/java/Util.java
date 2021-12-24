84c5b04996d1915f87ab474654c19eed546cb69d
ultraded <andrey.chernenkov@mail.ru>
2021-12-25 02:21:20 +0300

    Scripted auto-commit on change (2021-12-25 02:21:20) by gitwatch.sh

--- src/main/java/Util.java
+++ src/main/java/Util.java
@@ -1,10 +1,12 @@
 import scala.Tuple2;
 
 final public class Util {
-    private static final String AIRPORTS_DELIMITER = "\",", FLIGHTS_DELIMITER = ",";
+    private static final String AIRPORTS_DELIMITER = ",", FLIGHTS_DELIMITER = ",";
     private static final int ORIGIN_INDEX = 11, DESTINATION_INDEX = 14, DELAY_INDEX = 18, CANCELLED_INDEX = 19;
     private static int AIRPORTID_INDEX = 0, AIRPORTNAME_INDEX = 1;
     private static final float ZERO = 0.0f;
+
+    private static String removeremoveQuotes(String strWithQuotes) { return strWithQuotes.replaceAll("\'", ""); }
 
     public static boolean isCanceledOrDelayedFilter(String[] flight) {
         return ((Float.parseFloat(flight[CANCELLED_INDEX]) == ZERO)
@@ -12,7 +14,8 @@ final public class Util {
     }
 
     public static Tuple2<Integer,String> parseAirport(String airport) {
-        airport.split(AIRPORTS_DELIMITER);
+        String[] table = airport.split(AIRPORTS_DELIMITER);
+        int airportId = table[AIRPORTID_INDEX].replaceAll("\"", "");
     }
 
 
