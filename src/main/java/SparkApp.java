6c529453a43b76324426deac143c20dbe58a3560
ultraded <andrey.chernenkov@mail.ru>
2021-12-25 02:49:11 +0300

    Scripted auto-commit on change (2021-12-25 02:49:11) by gitwatch.sh

--- src/main/java/SparkApp.java
+++ src/main/java/SparkApp.java
@@ -2,6 +2,9 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import scala.Int;
+import scala.Tuple2;
+
 
 public class SparkApp {
 
@@ -12,6 +15,10 @@ public class SparkApp {
         JavaRDD<String> flights = sparkContext.textFile("664600583_T_ONTIME_sample.csv");
         JavaRDD<String> airports = sparkContext.textFile("L_AIRPORT_ID.csv");
 
-        JavaPairRDD<Integer, String> airportsRDD = airports.mapToPair()
+        JavaPairRDD<Integer, String> airportsRDD = airports.mapToPair(Util::parseAirport);
+        JavaPairRDD<Tuple2<Integer, Integer>, FlightSerializable> flightsRDD = flights.mapToPair(Util::parseFlight);
+
+        Jav
      }
 }
+
