3b8687d5b38326150d8a01f5e16fa1ca0f3bbbaa
ultraded <andrey.chernenkov@mail.ru>
2021-12-25 01:56:25 +0300

    Scripted auto-commit on change (2021-12-25 01:56:25) by gitwatch.sh

--- src/main/java/Util.java
+++ src/main/java/Util.java
@@ -1,5 +1,8 @@
 final public class Util {
-    private static final String AIRPOTSDELIMITER = "\",", FLIGHTSDELIMITER = ",";
-    private static final i
-    private static final String NULLSTR = "";
+    private static final String AIRPORTS_DELIMITER = "\",", FLIGHTS_DELIMITER = ",";
+    private static final int ORIGIN_INDEX = 11, DESTINATION_INDEX = 14, DELAY_INDEX = 17, CANCELLED_INDEX = 19;
+    private final int AIRPORTID_INDEX = 0, AIRPORTNAME_INDEX = 1;
+
+
+
 }
