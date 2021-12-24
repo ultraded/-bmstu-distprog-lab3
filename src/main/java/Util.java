cae5c6418d6d96df0f47f5f1c3df242d4816046c
ultraded <andrey.chernenkov@mail.ru>
2021-12-25 02:08:38 +0300

    Scripted auto-commit on change (2021-12-25 02:08:38) by gitwatch.sh

--- src/main/java/Util.java
+++ src/main/java/Util.java
@@ -1,11 +1,12 @@
 final public class Util {
-    final String AIRPORTS_DELIMITER = "\",", FLIGHTS_DELIMITER = ",";
+    private static final String AIRPORTS_DELIMITER = "\",", FLIGHTS_DELIMITER = ",";
     private static final int ORIGIN_INDEX = 11, DESTINATION_INDEX = 14, DELAY_INDEX = 18, CANCELLED_INDEX = 19;
-    private final int AIRPORTID_INDEX = 0, AIRPORTNAME_INDEX = 1;
-    private final float ZERO = 0.0f;
+    private static int AIRPORTID_INDEX = 0, AIRPORTNAME_INDEX = 1;
+    private static final float ZERO = 0.0f;
 
     public static boolean isCanceledOrDelayedFilter(String[] flight) {
-        return Float.parseFloat(flight[CANCELLED_INDEX]) == ZERO || Float.parseFloat(flight[DELAY_INDEX])
+        return ((Float.parseFloat(flight[CANCELLED_INDEX]) == ZERO)
+                || (Float.parseFloat(flight[DELAY_INDEX]) > ZERO));
     }
 
 
