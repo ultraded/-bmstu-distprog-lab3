adb6caf5caa6dbb561bd3cf4282868896b53b614
ultraded <andrey.chernenkov@mail.ru>
2021-12-25 02:26:44 +0300

    Scripted auto-commit on change (2021-12-25 02:26:44) by gitwatch.sh

--- src/main/java/Util.java
+++ src/main/java/Util.java
@@ -14,16 +14,22 @@ final public class Util {
                 || (Float.parseFloat(flight[DELAY_INDEX]) > ZERO));
     }
 
-    public static Tuple2<Integer,String> parseAirport(String airports) {
-        airports = removeQuotes(airports);
-        String[] table = airports.split(DELIMITER);
+    public static Tuple2<Integer,String> parseAirport(String airport) {
+        airport = removeQuotes(airport);
+        String[] table = airport.split(DELIMITER);
         int airportId = Integer.parseInt(table[AIRPORTID_INDEX]);
         String airportName = table[AIRPORTNAME_INDEX];
         return new Tuple2<>(airportId, airportName);
     }
 
     public static Tuple2<Tuple2<Integer, Integer>,FlightSerializable> parseFlight(String flight) {
-        
+        String[] table = flight.split(DELIMITER);
+        int destAirportID = Integer.parseInt(table[DESTINATION_INDEX]);
+        int originalAirportID = Integer.parseInt(table[ORIGIN_INDEX]);
+        float arrDelay = checkNull(table[ARRDELAY]);
+        float iscancelled = Float.parseFloat(table[CANCELLED]);
+        return new Tuple2<>(new Tuple2<>(originalAirportID, destAirportID),
+                new FlightSerializable(destAirportID, originalAirportID, arrDelay, iscancelled));
     }
 
 
