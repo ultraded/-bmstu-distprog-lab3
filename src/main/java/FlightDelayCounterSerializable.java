18a5425722a858d32ad42af6c73d99941c47b3ec
ultraded <andrey.chernenkov@mail.ru>
2021-12-25 02:31:21 +0300

    Scripted auto-commit on change (2021-12-25 02:31:21) by gitwatch.sh

--- src/main/java/FlightDelayCounterSerializable.java
+++ src/main/java/FlightDelayCounterSerializable.java
@@ -1,2 +1,36 @@
-public class FlightDelayCounterSerializable {
+import java.io.Serializable;
+
+public class FlightDelayCounterSerializable implements Serializable {
+    private float maxDelay;
+    private int totalFlights, delayedFlights, cancelledFlights;
+
+    public FlightDelayCounterSerializable() {
+    }
+
+    public FlightDelayCounterSerializable(float maxDelay, int totalFlights, int delayedFlights, int cancelledFlights) {
+        this.maxDelay = maxDelay;
+        this.totalFlights = totalFlights;
+        this.delayedFlights = delayedFlights;
+        this.cancelledFlights = cancelledFlights;
+
+
+    }
+
+    public float getMaxDelay() {
+        return maxDelay;
+    }
+
+    public int getTotalFlights() {
+        return totalFlights;
+    }
+
+    public int getDelayedFlights() {
+        return delayedFlights;
+    }
+
+    public int getCancelledFlights() {
+        return cancelledFlights;
+    }
+
+    public static add
 }
