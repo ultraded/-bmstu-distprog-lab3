f579dafada79f2077874ee3a825c0952b9c14769
ultraded <andrey.chernenkov@mail.ru>
2021-12-25 02:56:32 +0300

    Scripted auto-commit on change (2021-12-25 02:56:32) by gitwatch.sh

--- src/main/java/CounterSerializable.java
+++ src/main/java/CounterSerializable.java
@@ -1,13 +1,13 @@
 import java.io.Serializable;
 
-public class FlightDelayCounterSerializable implements Serializable {
+public class CounterSerializable implements Serializable {
     private float maxDelay;
     private int totalFlights, delayedFlights, cancelledFlights;
 
-    public FlightDelayCounterSerializable() {
+    public CounterSerializable() {
     }
 
-    public FlightDelayCounterSerializable(float maxDelay, int totalFlights, int delayedFlights, int cancelledFlights) {
+    public CounterSerializable(float maxDelay, int totalFlights, int delayedFlights, int cancelledFlights) {
         this.maxDelay = maxDelay;
         this.totalFlights = totalFlights;
         this.delayedFlights = delayedFlights;
@@ -32,21 +32,21 @@ public class FlightDelayCounterSerializable implements Serializable {
         return cancelledFlights;
     }
 
-    public static FlightDelayCounterSerializable addValue(FlightDelayCounterSerializable a, float maxArrDelay, boolean isDelayed, boolean isCancelled) {
-        return new FlightDelayCounterSerializable(a.getTotalFlights() + 1,
+    public static CounterSerializable addValue(CounterSerializable a, float maxArrDelay, boolean isDelayed, boolean isCancelled) {
+        return new CounterSerializable(a.getTotalFlights() + 1,
                 isDelayed ? a.getDelayedFlights() + 1 : a.getDelayedFlights(),
                 Math.max(a.getMaxDelay(), maxArrDelay),
                 isCancelled ? a.getDelayedFlights() + 1 : a.getDelayedFlights());
     }
 
-    public static FlightDelayCounterSerializable add(FlightDelayCounterSerializable a, FlightDelayCounterSerializable b) {
-        return new FlightDelayCounterSerializable(a.getTotalFlights() + b.getTotalFlights(),
+    public static CounterSerializable add(CounterSerializable a, CounterSerializable b) {
+        return new CounterSerializable(a.getTotalFlights() + b.getTotalFlights(),
                 a.getDelayedFlights() + b.getDelayedFlights(),
                 Math.max(a.getMaxDelay(), b.getMaxDelay()),
                 a.getDelayedFlights() + b.getDelayedFlights());
     }
 
-    public static String toOutString(FlightDelayCounterSerializable a) {
+    public static String toOutString(CounterSerializable a) {
         float percentOfDelays = (float) a.getDelayedFlights() / a.getTotalFlights() * 100;
         float percentOfCancelled = (float) a.getDelayedFlights() / a.getTotalFlights() * 100;
         return "INFO : { MaxDelay: " + a.getMaxDelay() +
--- src/main/java/SparkApp.java
+++ src/main/java/SparkApp.java
@@ -2,11 +2,8 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.broadcast.Broadcast;
 import scala.Int;
 import scala.Tuple2;
-
-import java.util.Map;
 
 
 public class SparkApp {
@@ -21,34 +18,6 @@ public class SparkApp {
         JavaPairRDD<Integer, String> airportsRDD = airports.mapToPair(Util::parseAirport);
         JavaPairRDD<Tuple2<Integer, Integer>, FlightSerializable> flightsRDD = flights.mapToPair(Util::parseFlight);
 
-        JavaPairRDD<Tuple2<Integer, Integer>, FlightSerCount> flightSerCounts =
-                dataOfAirportDelays
-                        .combineByKey(p -> new FlightSerCount(1,
-                                        p.getArrDelay() > ZERO ? 1 : 0,
-                                        p.getArrDelay(),
-                                        p.getCancelled() == ZERO ? 0 : 1),
-                                (flightSerCount, p) -> FlightSerCount.addValue(flightSerCount,
-                                        p.getArrDelay(),
-                                        p.getArrDelay() != ZERO,
-                                        p.getCancelled() != ZERO),
-                                FlightSerCount::add);
-     }
-    JavaPairRDD<Tuple2<Integer, Integer>, String> flightSerCountStrings = flightSerCounts
-            .mapToPair(value -> {
-                value._2();
-                return new Tuple2<>(value._1(), FlightSerCount.toOutString(value._2()));
-            });
-    final Broadcast<Map<Integer, String>> broadcast = sc.broadcast(dataOfAiportNames.collectAsMap());
-
-    JavaRDD<String> out = flightSerCountStrings.map(value -> {
-        Map<Integer, String> airportNames = broadcast.value();
-
-        String aiportNameOfStart = airportNames.get(value._1()._1());
-        String aiportNameOfFinish = airportNames.get(value._1()._2());
-
-        return aiportNameOfStart + " -> " + aiportNameOfFinish + "\n" + value._2();
-    });
-
-        out.saveAsTextFile("hdfs://localhost:9000/user/ultraded/output");
+       
 }
 
