1f9df828ae74b44633f365d50c353e4a5de637d5
ultraded <andrey.chernenkov@mail.ru>
2021-12-25 04:54:31 +0300

    Scripted auto-commit on change (2021-12-25 04:54:31) by gitwatch.sh

--- src/main/java/Util.java
+++ src/main/java/Util.java
@@ -1,24 +1,19 @@
-import scala.Int;
 import scala.Tuple2;
 
 final public class Util {
     private static final String DELIMITER = ",";
     private static final int ORIGIN_INDEX = 11, DESTINATION_INDEX = 14, DELAY_INDEX = 17, CANCELLED_INDEX = 19;
-    private static int AIRPORTID_INDEX = 0, AIRPORTNAME_INDEX = 1;
+    private static final int AIRPORTID_INDEX = 0, AIRPORTNAME_INDEX = 1;
     private static final float ZERO = 0.0f;
 
     private static String removeQuotes(String strWithQuotes) { return strWithQuotes.replaceAll("\'", ""); }
 
-    private static float checkNull(String current) {
+    private static float parseFloatWithNull(String current) {
         if (current.equals("")) {
             return ZERO;
         } else {
             return Float.parseFloat(current);
         }
-    }
-    public static boolean isCanceledOrDelayedFilter(String[] flight) {
-        return ((Float.parseFloat(flight[CANCELLED_INDEX]) == ZERO)
-                || (Float.parseFloat(flight[DELAY_INDEX]) > ZERO));
     }
 
     public static Tuple2<Integer,String> parseAirport(String airport) {
@@ -33,7 +28,7 @@ final public class Util {
         String[] table = flight.split(DELIMITER);
         int destAirportID = Integer.parseInt(table[DESTINATION_INDEX]);
         int originalAirportID = Integer.parseInt(table[ORIGIN_INDEX]);
-        float arrDelay = checkNull(table[DELAY_INDEX]);
+        float arrDelay = parseFloatWithNull(table[DELAY_INDEX]);
         float isCancelled = Float.parseFloat(table[CANCELLED_INDEX]);
         return new Tuple2<>(new Tuple2<>(originalAirportID, destAirportID),
                 new FlightSerializable(destAirportID, originalAirportID, arrDelay, isCancelled));
